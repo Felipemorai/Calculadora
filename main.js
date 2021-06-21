ac21cdf18b69f180fa736ea293053c995262a5fc
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-06-21 16:37:42 -0300

    committed

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 'use strict'
+
+const display = document.getElementById('display');
+const numbers = document.querySelectorAll('[id*=key]');
+
+const insertNumber = (event) => display.textContent = event.target.textContent;
+
+numbers.forEach (number =>
+    number.addEventListener('click',insertNumber)
+);
