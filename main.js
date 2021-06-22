e6f7eecd92b0a948a698e1cb7c03cadb9b073c0f
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-06-21 21:43:26 -0300

    committed

--- main.js
+++ main.js
@@ -2,9 +2,23 @@
 
 const display = document.getElementById('display');
 const numbers = document.querySelectorAll('[id*=key]');
+const operaters = document.querySelectorAll('[id*=operater]')
 
-const insertNumber = (event) => display.textContent = event.target.textContent;
+let newNumber = true;
+
+const updateDisplay = (text) => {
+    if (newNumber){
+        display.textContent = text;
+    }
+    else{
+        display.textContent += text;
+    }
+}
+
+const insertNumber = (event) => updateDisplay(event.target.textContent);
 
 numbers.forEach (number =>
     number.addEventListener('click',insertNumber)
 );
+
+operaters.forEach (operater => operater.addEventListener('click', selectOperater));
